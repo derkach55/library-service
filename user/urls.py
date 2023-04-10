bd817966581a1f622eea21dc7db08d09986ba0d2
Derkach Oleksandr <derkach03553@gmail.com>
2023-04-10 22:26:42 +0300

    added user views to urls

--- user/urls.py
+++ user/urls.py
@@ -5,10 +5,14 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
+from user.views import UserManageView, UserRegisterView
+
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('profile/', UserManageView.as_view(), name='profile'),
+    path('register/', UserRegisterView.as_view(), name='register'),
 ]
 
 app_name = 'user'
