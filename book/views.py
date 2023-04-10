add9d318bc492e2188a87e1cf525e13806ca4026
Derkach Oleksandr <derkach03553@gmail.com>
2023-04-10 17:42:30 +0300

    refactoring

--- book/views.py
+++ book/views.py
@@ -8,10 +8,10 @@ from book.serializers import AuthorSerializer, BookSerializer
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
-    permission_classes = IsAdminOrReadOnly
+    permission_classes = [IsAdminOrReadOnly]
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = IsAdminOrReadOnly
+    permission_classes = [IsAdminOrReadOnly]
