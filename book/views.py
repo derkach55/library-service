93aa2e95cbab9619b2fd656be8082840800699fc
Derkach Oleksandr <derkach03553@gmail.com>
2023-04-10 15:11:23 +0300

    add book, author view sets

--- book/views.py
+++ book/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from book.models import Author, Book
+from book.serializers import AuthorSerializer, BookSerializer
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
