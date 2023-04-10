f10a6c47041c5121489d64e54e9ac0e3d9397208
Derkach Oleksandr <derkach03553@gmail.com>
2023-04-10 13:41:11 +0300

    add book model

--- book/models.py
+++ book/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+
+class Book(models.Model):
+    COVER_CHOICES = ("HARD", "SOFT")
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, related_name="books", on_delete=models.CASCADE)
+    cover = models.CharField(choices=COVER_CHOICES, default="HARD")
+    inventory = models.PositiveIntegerField()
+    fee = models.DecimalField(max_digits=5, decimal_places=2)
