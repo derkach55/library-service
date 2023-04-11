6279444ab59d37474d3bd7f7788526b26657d052
Derkach Oleksandr <derkach03553@gmail.com>
2023-04-11 23:46:59 +0300

    add permissions

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,14 +1,14 @@
-import datetime
-
 from rest_framework import viewsets
 
 from borrowing.models import Borrowing
 from borrowing.serializers import BorrowingSerializer, BorrowingListSerializer, BorrowingRetrieveSerializer
+from book.permissions import IsAdminOrReadOnly
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+    permission_classes = IsAdminOrReadOnly
 
     def get_serializer_class(self):
         if self.action == "list":
