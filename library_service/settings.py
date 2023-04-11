5b8acc3bd4360a7cc9dd0457a7c4df3afa24215a
Derkach Oleksandr <derkach03553@gmail.com>
2023-04-11 16:33:02 +0300

    change jwt settings

--- library_service/settings.py
+++ library_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -43,6 +43,10 @@ INSTALLED_APPS = [
     'user',
     'borrowing'
 ]
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=20),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
