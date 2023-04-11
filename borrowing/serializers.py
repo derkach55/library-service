c138ce0d5aad46dfe6a4f190e715d9de06a00926
Derkach Oleksandr <derkach03553@gmail.com>
2023-04-11 23:43:49 +0300

    add date validation

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -1,4 +1,7 @@
+import datetime
+
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from book.serializers import BookSerializer
 from borrowing.models import Borrowing
@@ -10,6 +13,15 @@ class BorrowingSerializer(serializers.ModelSerializer):
         model = Borrowing
         fields = ('id', 'expected_return', 'actual_return', 'borrow_date', 'book', 'user')
         read_only_fields = ('id', 'user', 'actual_return', 'borrow_date')
+
+    def validate(self, attrs):
+        borrow_date = attrs.get('borrow_date', datetime.date.today())
+        if attrs['expected_return'] < borrow_date:
+            raise ValidationError('Expected return date should be after the borrow date.')
+        actual_return = attrs.get('actual_return', None)
+        if actual_return and borrow_date > actual_return:
+            raise ValidationError('Actual return date should be after the borrow date.')
+        return attrs
 
 
 class BorrowingListSerializer(BorrowingSerializer):
