e28cf3ffdcbfca1ade574d0253aedafa400620bc
Derkach Oleksandr <derkach03553@gmail.com>
2023-04-11 16:00:43 +0300

    add borrowing urls

---
+++ borrowing/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from borrowing.views import BorrowingViewSet
+
+router = DefaultRouter()
+router.register('borrowings', BorrowingViewSet)
+
+urlpatterns = [path('', include(router.urls))]
+app_name = 'borrowing'
