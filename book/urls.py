f0fef3074fa4592f71d7dd7fc92181bbce396ea1
Derkach Oleksandr <derkach03553@gmail.com>
2023-04-11 14:13:41 +0300

    removed author model

--- book/urls.py
+++ book/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from book.views import BookViewSet, AuthorViewSet
+from book.views import BookViewSet
 
 router = DefaultRouter()
 router.register('books', BookViewSet)
-router.register('authors', AuthorViewSet)
 
 urlpatterns = [path('', include(router.urls))]
 app_name = 'book'
