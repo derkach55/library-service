67f1c3354a991c999cfce0e2607a4f38628a0237
Derkach Oleksandr <derkach03553@gmail.com>
2023-04-17 21:00:24 +0300

    added check overdues task

--- borrowing/tasks.py
+++ borrowing/tasks.py
@@ -11,4 +11,18 @@ def borrowing_alert(email, title, expected_return):
     send_to_telegram(message)
 
 
-
+@shared_task
+def check_overdue():
+    borrowings = [
+        {
+            'user': borrowing.user.email,
+            'book': borrowing.book.title,
+            'overdue': (datetime.date.today() - borrowing.expected_return).days
+        }
+        for borrowing in Borrowing.objects.filter(
+            actual_return__isnull=True,
+            expected_return__lt=datetime.date.today()
+        )
+    ]
+    for borrowing in borrowings:
+        send_to_telegram(f"{borrowing['user']} overdue {borrowing['book']} for {borrowing['overdue']} days")
