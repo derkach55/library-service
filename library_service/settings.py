703a9814078206c40292fc32f008fa692eeb358c
Derkach Oleksandr <derkach03553@gmail.com>
2023-04-18 14:10:42 +0300

    added periodic task

--- library_service/settings.py
+++ library_service/settings.py
@@ -140,6 +140,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CELERY_BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BEAT_SCHEDULE = {
+    'check-overdue-every-day': {
+        'task': 'borrowing.tasks.check_overdue',
+        'schedule': timedelta(days=1),
+    },
+}
 CELERY_TIMEZONE = 'Europe/Kiev'
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
