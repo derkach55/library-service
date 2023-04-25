cb707a97c4a07df43a5bbdcffb430428773a9f78
Derkach Oleksandr <derkach03553@gmail.com>
2023-04-25 13:47:21 +0300

    added list, retrieve tests

--- book/tests.py
+++ book/tests.py
@@ -3,18 +3,33 @@ from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
 
+from book.models import Book
+from book.serializers import BookSerializer
+
 BOOK_URL = reverse('book:book-list')
 
 
 class TestUnauthenticatedBookApi(APITestCase):
     def setUp(self) -> None:
         self.client = self.client_class()
+        self.book1 = Book.objects.create(title='test', author='test', cover='SOFT', inventory=10, fee='1.00')
+        self.book2 = Book.objects.create(title='test2', author='test2', cover='HARD', inventory=20, fee='2.00')
 
     def test_auth_required(self):
-        res = self.client.get(BOOK_URL)
-        self.assertEqual(res.status_code, 200)
         res2 = self.client.post(BOOK_URL)
         self.assertEqual(res2.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_list_book(self):
+        res = self.client.get(BOOK_URL)
+        serializer = BookSerializer(Book.objects.all(), many=True)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_retrieve_book(self):
+        res = self.client.get(reverse('book:book-detail', args=[1]))
+        serializer = BookSerializer(self.book1)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.data, serializer.data)
 
 
 class TestAuthenticatedBookApi(APITestCase):
